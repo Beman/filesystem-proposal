768cf2de3ef536aa706d6ab8beb90df1f0229182
Beman <bdawes@acm.org>
2012-06-11 08:54:30 -0400

    Add section number generation to proposal

--- doc/src/build.bat
+++ doc/src/build.bat
@@ -1,5 +1,6 @@
 @echo off
 del tr2.html 2>nul
-mmp TARGET=TR2 source.html tr2.html
+mmp TARGET=TR2 source.html temp.html
+html_section_numbers <temp.html >lwg_proposal.html
 del reference.html 2>nul
 mmp TARGET=BOOST source.html reference.html
