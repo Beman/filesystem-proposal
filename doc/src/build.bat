73baffaadeaad8bad06f8d33d7f264d7ec1b58c6
Beman <bdawes@acm.org>
2012-06-11 09:30:34 -0400

    Replace uses of  const string return types with non-const string return types, per LWG discussion in Kona. Update Revision history.

--- doc/src/build.bat
+++ doc/src/build.bat
@@ -1,6 +1,6 @@
 @echo off
 del tr2.html 2>nul
 mmp TARGET=TR2 source.html temp.html
-html_section_numbers <temp.html >lwg_proposal.html
+html_section_numbers <temp.html >filesystem-proposal.html
 del reference.html 2>nul
 mmp TARGET=BOOST source.html reference.html
