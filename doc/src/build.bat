1defcf924d87c8247808e6f2a196a877b50c5b51
Beman <bdawes@acm.org>
2012-07-17 08:07:07 -0400

    Merge changes from svn trunk 79574 July 17, 2012

--- doc/src/build.bat
+++ doc/src/build.bat
@@ -1,6 +1,9 @@
 @echo off
+rem Copyright Beman Dawes 2012
+rem Distributed under the Boost Software License, Version 1.0.
 del tr2.html 2>nul
 mmp TARGET=TR2 source.html temp.html
 html_section_numbers <temp.html >filesystem-proposal.html
 del reference.html 2>nul
 mmp TARGET=BOOST source.html reference.html
+echo run "hoist" to hoist reference.html to doc directory
---
+++ doc/src/copy-gh-pages.bat
@@ -0,0 +1,1 @@
+copy /y tr2.html C:\j16\dot16-gh-pages\filesystem-proposal.html
