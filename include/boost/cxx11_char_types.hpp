48fbb0bb66602bb801fbf62c7fa84e2dcae619e5
Beman <bdawes@acm.org>
2012-07-19 08:48:01 -0400

    Edit comments.

--- include/boost/cxx11_char_types.hpp
+++ include/boost/cxx11_char_types.hpp
@@ -1,4 +1,4 @@
-//  boost/cxx11_chars.hpp  --------------------------------------------------------//
+//  boost/cxx11_chars.hpp  -------------------------------------------------------------//
 
 //  Copyright Beman Dawes 2011
 
@@ -7,17 +7,30 @@
 
 //--------------------------------------------------------------------------------------//
 //                                                                                      //
-//   In namespace boost, provide typedefs for C++11 types if present, otherwise for     //
-//   C++03 equivalent types. By using the boost typedefs, these C++11 features become   //
-//   available for use with C++03 compilers.                                            //
+//  The purpose of this header is to emulate the new C++11 char16_t and char32_t        //
+//  character and string types so that they can be used in both C++11 and C++03         //
+//  programs.                                                                           //
 //                                                                                      //
+//  The emulation names use char16/char32 rather than char16_t/char32_t to avoid use    //
+//  of names that are keywords in C++11.                                                //
+//                                                                                      //
+//  The emulation names are placed in namespace boost, as is usual for Boost C++11      //
+//  emulation names such as those in header <boost/cstdint.hpp>.                        //
+//                                                                                      //
+//  An alternative would would have been to place the C++11 emulation names at global   //
+//  scope, and put the C++11 string types in namespace std. That is the approach taken  //
+//  by Microsoft Visual Studio 2010, but is controversion with some Boost users and     //
+//  developers, and runs counter to usual Boost practice.                               //
+//                                                                                      //
+//  Thanks to Mathias Gaunard and others for discussions leading to the final form      //
+//  of these typedefs.                                                                  //
 //                                                                                      //
 //   Boost               C++11            C++03                                         //
 //   ----------------    --------------   --------------------------------              //
-//   boost::char16  char16_t         uint16_t                                      //
-//   boost::char32  char32_t         uint32_t                                      //
-//   boost::u16string    std::u16string   std::basic_string<boost::char16>         //
-//   boost::u32string    std::u32string   std::basic_string<boost::char32>         //
+//   boost::char16       char16_t         uint16_t                                      //
+//   boost::char32       char32_t         uint32_t                                      //
+//   boost::u16string    std::u16string   std::basic_string<boost::char16>              //
+//   boost::u32string    std::u32string   std::basic_string<boost::char32>              //
 //                                                                                      //
 //   Uses the typedefs provided by Microsoft Visual C++ 2010 if present                 //
 //                                                                                      //
@@ -32,29 +45,6 @@
 # include <boost/config.hpp>
 # include <boost/cstdint.hpp>
 # include <string>
-
-//--------------------------------------------------------------------------------------//
-//                                                                                      
-//  Naming and namespace rationale
-//
-//  The purpose of this header is to emulate the new C++11 char16_t and char32_t
-//  character and string types so that they can be used in C++03 programs.
-//
-//  The emulation names use char16/char32 rather than char16_t/char32_t to avoid use of
-//  names that are keywords in C++11.
-//
-//  The emulation names are placed in namespace boost, as is usual for Boost C++11
-//  emulation names such as those in header <boost/cstdint.hpp>.
-//
-//  An alternative would would have been to place the C++11 emulation names at global
-//  scope, and put the C++11 string types in namespace std. That is the approach taken
-//  by Microsoft Visual Studio 2010, but is controversion with some Boost users and
-//  developers, and runs counter to usual Boost practice.  
-//
-//  Thanks to Mathias Gaunard and others for discussions leading to the final form
-//  of these typedefs.
-//
-//--------------------------------------------------------------------------------------//
 
 namespace boost
 {
