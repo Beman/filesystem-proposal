5b1df556dfb2c2e2317dbc5ba2b0d254d79fcc76
Beman <bdawes@acm.org>
2012-08-11 17:16:33 -0400

    Updated table of contents

--- doc/src/build.bat
+++ doc/src/build.bat
@@ -1,9 +1,28 @@
 @echo off
 rem Copyright Beman Dawes 2012
 rem Distributed under the Boost Software License, Version 1.0.
-del tr2.html 2>nul
+
+echo Generating LWG filesystem-proposal.html...
+del filesystem-proposal.html 2>nul
+
+rem ' dir="ltr"' messes up the section number generation
+chg boost_snippets.html " dir=\qltr\q" ""
+chg tr2_snippets.html " dir=\qltr\q" ""
+chg source.html " dir=\qltr\q" ""
+
 mmp TARGET=TR2 source.html temp.html
 html_section_numbers <temp.html >filesystem-proposal.html
+toc -x filesystem-proposal.html filesystem-proposal.toc
+echo Fresh table-of-contents generated in filesystem-proposal.toc.
+echo Hand edit into source files if desired.
+type \bgd\util\crnl.txt
+
+echo Generating Boost reference.html...
 del reference.html 2>nul
 mmp TARGET=BOOST source.html reference.html
-echo run "hoist" to hoist reference.html to doc directory
+toc -x reference.html reference.toc
+echo Fresh table-of-contents generated in reference.toc.
+echo Hand edit into source files if desired.
+type \bgd\util\crnl.txt
+
+echo Run "hoist" to hoist reference.html to doc directory
