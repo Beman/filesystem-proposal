667e2f1b61c3d4599acf3f26903f584ebd8b14bf
Beman <bdawes@acm.org>
2012-08-13 11:43:05 -0400

    Update for name changes

--- doc/src/copy-gh-pages.bat
+++ doc/src/copy-gh-pages.bat
@@ -1,6 +1,5 @@
 copy /y filesystem-proposal.html D:\filesystem-proposal\index.html
-pushd D:\filesystem-proposal-mods
-git commit -a -m "auto commit"
-git push origin gh-pages
+pushd D:\filesystem-proposal
+call git commit -a -m "auto commit"
+call git push origin gh-pages
 popd
-
