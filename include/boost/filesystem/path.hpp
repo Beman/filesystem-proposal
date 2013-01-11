2725dadd1a87383eecd4eb37ba9d9ae55337dde6
Beman <bdawes@acm.org>
2013-01-11 08:14:48 -0500

    Revert "Add "Design decisions for paths" section."
    This reverts commit bc482f0dbae3334e10e2e61ed6e84b6ca68f9d3e for path.hpp
    
    Then add "Design decisions for paths" section docs changes

--- include/boost/filesystem/path.hpp
+++ include/boost/filesystem/path.hpp
@@ -40,7 +40,6 @@
 #include <cassert>
 #include <locale>
 #include <algorithm>
-#include <cvt/wstring>
 
 #include <boost/config/abi_prefix.hpp> // must be the last #include
 
@@ -194,10 +193,6 @@ namespace filesystem
         path_traits::convert(s.c_str(), s.c_str()+s.size(), m_pathname, cvt);
       }
     }
-
-    //  Filesystem TS constructors
-    path(const std::string& s, const std::locale& loc);
-    path(const char* s, const std::locale& loc);
 
     //  -----  assignments  -----
 
